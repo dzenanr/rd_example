9fb0963fc7fdea7fd63175d8fa305ff4da4c112d
Dzenan Ridjanovic <dzenanr@gmail.com>
2013-09-18 17:28:38 -0400

    initial version

---
+++ README.md
@@ -0,0 +1,10 @@
+# rd_example
+
+**Categories**: education, library, random.
+
+**Description**:
+Example of using the rd (random data) library.
+
+**Start**:
+Clone the rd_example project and open it with Dart Editor.
+Pub Update the pubspec.yaml file.
---
+++ bin/rd_example.dart
@@ -0,0 +1,10 @@
+import 'package:rd/rd.dart';
+
+main() {
+  var list = [23, 45, 12, 3, 56, 99];
+  var rn = randomListElement(list);
+  print('Example of using rd library');
+  print('');
+  print('input list: $list');
+  print('random number from list: $rn');
+}
